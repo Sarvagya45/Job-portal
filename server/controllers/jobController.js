179da15f6ecc4a6ce6ba286e3e9a8b3b7b64a834
gsarvagya45 <gsarvagya45@gmail.com>
2025-07-07 18:57:06 +0530

    job by id

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -6,9 +6,13 @@ export const getJobs = async (req, resp) =>{
      try {
         
         const jobs = await Job.find({visible:true})
-        .populate({path:'company',select:"-password"})
+        .populate({path:'companyId',select:"-password"})
         
         resp.json({success:true, jobs})
+
+         if(!jobs){
+            return resp.json({success:false, message:"jobs not found"})
+        }
 
      } catch (error) {
         resp.json({success:false, message:error.message})
@@ -17,5 +21,20 @@ export const getJobs = async (req, resp) =>{
 
 //get a single job by ID
 export const getJobById = async (req, resp) =>{
+    try {
 
+        const {id} = req.params
+
+        const job = await Job.findById(id)
+        .populate({path:"companyId",select:"-password"})
+
+        resp.json({success:true, job})
+
+        if(!job){
+            return resp.json({success:false, message:"job not found"})
+        }
+
+    } catch (error) {
+        resp.json({success:false,message:"error.message"})
+    }
 }
